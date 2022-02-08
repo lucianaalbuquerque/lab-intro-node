a59a30ac64db48ff273e13148e24863aa940d281
Luciana A <lucianaalbuquerque12@gmail.com>
2022-02-08 12:28:44 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return  this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+   
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return this.items[this.items.length-1]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items[0]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a,b)=>a+b)
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0) {
+      return this.sum() / this.items.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
+
 
 module.exports = SortedList;
